c6f71f3e495cedbe9422d9bb15f07a91215ef1af
falkheiland <falkheiland@gmail.com>
2018-04-04 15:38:04 +0200

    rewrite for db and db user usage

--- PSIGEL/Public/Get-UMSThinclient.ps1
+++ PSIGEL/Public/Get-UMSThinclient.ps1
@@ -10,19 +10,25 @@
       .PARAMETER ServerInstance
       SQL ServerInstance  for the UMS-DB (e.g. 'SQLSERVER\RMDB')
 
+      .PARAMETER Database
+      SQL Database  for the UMS-DB (e.g. 'RMDB')
+
+      .PARAMETER DatabaseUser
+      SQL DatabaseUser  for the UMS-DB (e.g. 'igelums')
+
       .PARAMETER TCIDColl
       ThinclientIDs to search for
 
       .EXAMPLE
-      Get-UMSThinclient -ServerInstance 'SQLSERVER\RMDB' | Out-GridView
+      Get-UMSThinclient -ServerInstance 'SQLSERVER\RMDB' -Database 'RMDB' -DatabaseUser 'igelums' | Out-GridView
       Gets all Thinclients
       
       .EXAMPLE
-      Get-UMSThinclient -ServerInstance 'SQLSERVER\RMDB' -TCIDColl 607377
+      Get-UMSThinclient -ServerInstance 'SQLSERVER\RMDB' -Database 'RMDB' -DatabaseUser 'igelums' -TCIDColl 2433
       Gets Thinclient with TCID "607377"
       
       .EXAMPLE
-      607377, 680819 | Get-UMSThinclient -ServerInstance 'SQLSERVER\RMDB'
+      607377, 680819 | Get-UMSThinclient -ServerInstance 'SQLSERVER\RMDB' -Database 'RMDB' -DatabaseUser 'igelums'
       Gets Thinclient with TCID "607377" and "680819" 
   #>
   
@@ -32,6 +38,14 @@
     [Parameter(Mandatory)]
     [String]
     $ServerInstance,
+    
+    [Parameter(Mandatory)]
+    [String]
+    $Database,
+    
+    [Parameter(Mandatory)]
+    [String]
+    $DatabaseUser,
     
     [Parameter(ValueFromPipeline)]
     [int]
@@ -47,14 +61,14 @@
     {
       0
       {
-        $Query = @'
+        $Query = @"
 SELECT *
-FROM [rmdb].[igelums].[THINCLIENT] TC, [rmdb].[igelums].[THINCLIENTISINDIRECTORY] TD, [rmdb].[igelums].[FIRMWARE] FW
+FROM [$Database].[$DatabaseUser].[THINCLIENT] TC, [$Database].[$DatabaseUser].[THINCLIENTISINDIRECTORY] TD, [$Database].[$DatabaseUser].[FIRMWARE] FW
 WHERE TC.TCID = TD.TCID
 AND TC.FIRMWAREID = FW.FIRMWAREID
 AND MOVEDTOBIN IS NULL
-'@
-        Invoke-Sqlcmd2 -ServerInstance $ServerInstance -Query $Query
+"@
+        Invoke-Sqlcmd2 -ServerInstance $ServerInstance -Database $Database -Query $Query
       }
       default
       {
@@ -62,13 +76,13 @@ AND MOVEDTOBIN IS NULL
         {
           $Query = (@"
 SELECT *
-FROM [rmdb].[igelums].[THINCLIENT] TC, [rmdb].[igelums].[THINCLIENTISINDIRECTORY] TD, [rmdb].[igelums].[FIRMWARE] FW
+FROM [$Database].[$DatabaseUser].[THINCLIENT] TC, [$Database].[$DatabaseUser].[THINCLIENTISINDIRECTORY] TD, [$Database].[$DatabaseUser].[FIRMWARE] FW
 WHERE TC.TCID = TD.TCID
 AND TC.FIRMWAREID = FW.FIRMWAREID
 AND TC.TCID = '{0}'
 AND MOVEDTOBIN IS NULL
 "@ -f $TCID)
-          Invoke-Sqlcmd2 -ServerInstance $ServerInstance -Query $Query
+          Invoke-Sqlcmd2 -ServerInstance $ServerInstance -Database $Database -Query $Query
         }
       }
     }
@@ -77,4 +91,3 @@ AND MOVEDTOBIN IS NULL
   {
   }
 }
-
