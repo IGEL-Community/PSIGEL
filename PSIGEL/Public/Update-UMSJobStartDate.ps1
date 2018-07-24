1ae93a4f3a28c529c7df4fc3876999a26c3e0e9e
falkheiland <falkheiland@gmail.com>
2018-07-24 17:03:40 +0200

    .Updated SQL query

--- PSIGEL/Public/Update-UMSJobStartDate.ps1
+++ PSIGEL/Public/Update-UMSJobStartDate.ps1
@@ -8,7 +8,17 @@
       Update Job Startdate in UMS-DB
 
       .PARAMETER ServerInstance
-      SQL ServerInstance  for the UMS-DB (e.g. 'SQLSERVER\RMDB')
+      SQL ServerInstance for the UMS-DB (e.g. 'SQLSERVER\RMDB')
+
+      .PARAMETER Database
+      SQL Database  for the UMS-DB (e.g. 'RMDB')
+
+      .PARAMETER Schema
+      SQL Schema  for the UMS-DB (e.g. 'igelums')
+
+      .PARAMETER Credential
+      Specifies A PSCredential for SQL Server Authentication connection to an instance of the Database Engine.
+      If -Credential is not specified, Invoke-Sqlcmd attempts a Windows Authentication connection using the Windows account running the PowerShell session.
 
       .PARAMETER JobIDColl
       Array of Job IDs
@@ -17,20 +27,39 @@
       Startdate to set in Format 'yyyy-MM-dd 00:00:00'
 
       .EXAMPLE
-      Update-UMSJobStartDate -ServerInstance 'SQLSERVER\RMDB' -JobIDColl 513339 -Startdate '2018-01-10 14:47:00'
-      #Update Job Startdate on Job with ID "607377"
+      $Params = @{
+        ServerInstance = 'SQLSERVER\RMDB'
+        Database       = 'RMDB'
+        Schema         = 'igelums'
+        Credential     = (Get-Credential)
+        JobIDColl      = 18701
+        Startdate      = '2018-01-10 00:06:41'
+      }
+      Update-UMSJobStartDate @Params
+      #Update Job Startdate on Job with ID "18701"
 
       .EXAMPLE
-      513339,515266 | Update-UMSJobStartDate -ServerInstance 'SQLSERVER\RMDB' -Startdate '2018-01-10 14:47:00'
-      #Update Job Startdate on Jobs with ID "607377" and "680819"
+      18701,18706 | Update-UMSJobStartDate -ServerInstance 'SQLSERVER\RMDB' -Database 'RMDB' -Schema 'igelums' -Startdate '2018-01-10 14:47:00'
+      #Update Job Startdate on Jobs with ID "18701" and "18706"
   #>
 
-  [cmdletbinding(SupportsShouldProcess, ConfirmImpact = 'Low')]
+  [cmdletbinding(SupportsShouldProcess, ConfirmImpact = 'Medium')]
   param
   (
     [Parameter(Mandatory)]
     [String]
     $ServerInstance,
+
+    [Parameter(Mandatory)]
+    [String]
+    $Database,
+
+    [Parameter(Mandatory)]
+    [String]
+    $Schema,
+
+    [PSCredential]
+    $Credential,
 
     [Parameter(Mandatory, ValueFromPipeline)]
     [int[]]
@@ -46,17 +75,32 @@
   }
   Process
   {
+    if ($Credential)
+    {
+      $InvokeSqlcmd2Params = @{
+        ServerInstance = $ServerInstance
+        Database       = $Database
+        Credential     = $Credential
+      }
+    }
+    else
+    {
+      $InvokeSqlcmd2Params = @{
+        ServerInstance = $ServerInstance
+        Database       = $Database
+      }
+    }
     foreach ($JobID in $JobIDColl)
     {
       $Query = (@"
-UPDATE [rmdb].[igelums].[JOB]
-SET [STARTDATE] = '{0}'
-WHERE [ID] = '{1}'
-"@ -f $Startdate, $JobID)
+UPDATE [{0}].[{1}].[JOB]
+SET [STARTDATE] = '{2}'
+WHERE [ID] = '{3}'
+"@ -f $Database, $Schema, $Startdate, $JobID)
     }
     if ($PSCmdlet.ShouldProcess('JobID: {0}' -f $JobID))
     {
-      Invoke-Sqlcmd2 -ServerInstance $ServerInstance -Query $Query
+      Invoke-Sqlcmd2 @InvokeSqlcmd2Params -Query $Query
     }
   }
   End
