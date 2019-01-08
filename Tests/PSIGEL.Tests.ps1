0a58706ef386cff7a2dad4acfcdf85937bf310e2
falkheiland <falkheiland@users.noreply.github.com>
2019-01-08 13:58:08 +0100

    Update PSIGEL.Tests.ps1

--- Tests/PSIGEL.Tests.ps1
+++ Tests/PSIGEL.Tests.ps1
@@ -7,9 +7,9 @@ $ModuleManifest = Resolve-Path ('{0}/{1}.psd1' -f $ModuleRoot, $ModuleName)
 Describe "General project validation: $ModuleName" {
 
   Context 'Basic Module Testing' {
-    $Scripts = Get-ChildItem $ModuleRoot -Include *.ps1, *.psm1, *.psd1 -Recurse
+    $ScriptColl = Get-ChildItem $ModuleRoot -Include *.ps1, *.psm1, *.psd1 -Recurse
 
-    $TestCase = $Scripts | Foreach-Object {
+    $TestCase = $ScriptColl | Foreach-Object {
       @{
         File = $_
       }
@@ -59,22 +59,22 @@ Describe "General project validation: $ModuleName" {
   }
 
   Context 'Public Functions' {
-    $PublicFunctions = (Get-ChildItem -Path ('{0}\Public' -f $ModuleRoot) -Filter *.ps1 |
+    $PublicFunctionColl = (Get-ChildItem -Path ('{0}\Public' -f $ModuleRoot) -Filter *.ps1 |
         Select-Object -ExpandProperty Name ) -replace '\.ps1$'
 
-    $TestCase = $PublicFunctions | Foreach-Object {
+    $TestCase = $PublicFunctionColl | Foreach-Object {
       @{
         FunctionName = $_
       }
     }
 
-    It "Function <FunctionName> should be in manifest" -TestCases $testCase {
+    It "Function <FunctionName> should be in manifest" -TestCases $TestCase {
       param(
         $FunctionName
       )
 
-      $ManifestFunctions = $Manifest.ExportedFunctions.Keys
-      $FunctionName -in $ManifestFunctions | Should Be $true
+      $ManifestFunctionColl = $Manifest.ExportedFunctions.Keys
+      $FunctionName -in $ManifestFunctionColl | Should Be $true
     }
 
     It 'Proper Number of Functions Exported compared to Manifest' {
@@ -93,21 +93,104 @@ Describe "General project validation: $ModuleName" {
     }
   }
 
-  Context 'Private
-   Functions' {
-    $PrivateFunctions = (Get-ChildItem -Path ('{0}\Private' -f $ModuleRoot) -Filter *.ps1 |
+  Context 'Private Functions' {
+    $PrivateFunctionColl = (Get-ChildItem -Path ('{0}\Private' -f $ModuleRoot) -Filter *.ps1 |
         Select-Object -ExpandProperty Name ) -replace '\.ps1$'
-    $TestCase = $PrivateFunctions | Foreach-Object {
+    $TestCase = $PrivateFunctionColl | Foreach-Object {
       @{
         FunctionName = $_
       }
     }
 
-    It "Private function <FunctionName> is not directly accessible outside the module" -TestCases $testCase {
+    It "Private function <FunctionName> is not directly accessible outside the module" -TestCases $TestCase {
       param(
         $FunctionName
       )
       { . ('\{0}' -f $FunctionName) } | Should Throw
+    }
+  }
+
+  Context 'Exported Aliases' {
+    It 'Proper Number of Aliases Exported compared to Manifest' {
+      $ExportedCount = Get-Command -Module $ModuleName -CommandType Alias |
+        Measure-Object | Select-Object -ExpandProperty Count
+      $ManifestCount = $Manifest.ExportedAliases.Count
+
+      $ExportedCount | Should be $ManifestCount
+    }
+
+    It 'Proper Number of Aliases Exported compared to Files' {
+      $AliasCount = Get-ChildItem -Path "$ModuleRoot\Public" -Filter *.ps1 |
+        Select-String "New-Alias" | Measure-Object | Select-Object -ExpandProperty Count
+      $ManifestCount = $Manifest.ExportedAliases.Count
+
+      $AliasCount  | Should be $ManifestCount
+    }
+  }
+}
+
+Describe "$ModuleName ScriptAnalyzer" -Tag 'Compliance' {
+  $PSScriptAnalyzerSettingColl = @{
+    Severity    = @('Error', 'Warning')
+    #ExcludeRule = @('PSUseSingularNouns')
+    ExcludeRule = @('')
+  }
+  # Test all functions with PSScriptAnalyzer
+  $ScriptAnalyzerErrorColl = @()
+  $ScriptAnalyzerErrorColl += Invoke-ScriptAnalyzer -Path "$ModuleRoot\Public" @PSScriptAnalyzerSettingColl
+  $ScriptAnalyzerErrorColl += Invoke-ScriptAnalyzer -Path "$ModuleRoot\Private" @PSScriptAnalyzerSettingColl
+  # Get a list of all internal and Exported functions
+  $PrivateFunctionColl = Get-ChildItem -Path "$ModuleRoot\Private" -Filter *.ps1 |
+    Select-Object -ExpandProperty Name
+  $PublicFunctionColl = Get-ChildItem -Path "$ModuleRoot\Public" -Filter *.ps1 |
+    Select-Object -ExpandProperty Name
+  $AllFunctionColl = ($PrivateFunctionColl + $PublicFunctionColl) | Sort-Object
+  $FunctionWithErrorColl = $ScriptAnalyzerErrorColl.ScriptName | Sort-Object -Unique
+  if ($ScriptAnalyzerErrorColl)
+  {
+    $TestCase = $ScriptAnalyzerErrorColl | Foreach-Object {
+      @{
+        RuleName   = $_.RuleName
+        ScriptName = $_.ScriptName
+        Message    = $_.Message
+        Severity   = $_.Severity
+        Line       = $_.Line
+      }
+    }
+    # Compare those with not successfull
+    $FunctionWithoutErrorColl = Compare-Object -ReferenceObject $AllFunctionColl -DifferenceObject $FunctionWithErrorColl |
+      Select-Object -ExpandProperty InputObject
+    Context 'ScriptAnalyzer Testing' {
+      It "Function <ScriptName> should not use <Message> on line <Line>" -TestCases $TestCase {
+        param(
+          $RuleName,
+          $ScriptName,
+          $Message,
+          $Severity,
+          $Line
+        )
+        $ScriptName | Should BeNullOrEmpty
+      }
+    }
+  }
+  else
+  {
+    # Everything was perfect, let's show that as well
+    $FunctionWithoutErrorColl = $AllFunctionColl
+  }
+
+  # Show good functions in the test, the more green the better
+  Context 'Successful ScriptAnalyzer Testing' {
+    $TestCase = $FunctionWithoutErrorColl | Foreach-Object {
+      @{
+        ScriptName = $_
+      }
+    }
+    It "Function <ScriptName> has no ScriptAnalyzerErrors" -TestCases $TestCase {
+      param(
+        $ScriptName
+      )
+      $ScriptName | Should Not BeNullOrEmpty
     }
   }
 }
