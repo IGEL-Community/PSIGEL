efe944fe05666087716e11c29405dee6dca27970
falkheiland <falkheiland@users.noreply.github.com>
2019-01-10 13:23:25 +0100

    Update Get-EPFirmware.Tests.ps1

--- Tests/Get-EPFirmware.Tests.ps1
+++ Tests/Get-EPFirmware.Tests.ps1
@@ -2,30 +2,43 @@ $Script:ProjectRoot = Resolve-Path ('{0}\..' -f $PSScriptRoot)
 $Script:ModuleRoot = Split-Path (Resolve-Path ('{0}\*\*.psm1' -f $Script:ProjectRoot))
 $Script:ModuleName = Split-Path $Script:ModuleRoot -Leaf
 $Script:ModuleManifest = Resolve-Path ('{0}/{1}.psd1' -f $Script:ModuleRoot, $Script:ModuleName)
-$Script:CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+$Script:FunctionName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 
-Describe "$Script:CommandName Unit Tests" -Tag 'UnitTests' {
+Describe "$Script:FunctionName Unit Tests" -Tag 'UnitTests' {
+  BeforeAll {
+    Get-Module -All -Name $Script:ModuleName | Remove-Module -Force -ErrorAction 'Ignore'
+    Import-Module ( '{0}/{1}.psm1' -f $Script:ModuleRoot, $Script:ModuleName) -Force
+    $Global:ConfirmPreference = 'None'
+  }
   Context "Validate parameters" {
-    BeforeAll {
-      Get-Module -All -Name $Script:ModuleName | Remove-Module -Force -ErrorAction 'Ignore'
-      Import-Module ( '{0}/{1}.psm1' -f $Script:ModuleRoot, $Script:ModuleName) -Force
-      $Global:ConfirmPreference = 'None'
-    }
-
-    [object[]]$params = (Get-ChildItem function:\$Script:CommandName).Parameters.Keys
+    
+    [object[]]$params = (Get-ChildItem function:\$Script:FunctionName).Parameters.Keys
     $KnownParameters = 'SSHSession'
-
+    
     It "Should contain our specific parameters" {
       (@(Compare-Object -ReferenceObject $KnownParameters -DifferenceObject $params -IncludeEqual |
             Where-Object SideIndicator -eq "==").Count) | Should Be $KnownParameters.Count
     }
+  }
+  
+  Context "xyz" {
+    
+    $PSDefaultParameterValues = @{
+      '*:SSHSession' = New-MockObject -Type 'SSH.SshSession'
+    }
 
-    Mock -CommandName 'Invoke-SSHCommandStream' -MockWith {'ddd'}
+    Mock 'Invoke-SSHCommandStream' {'4.14.300.01 '}
+    
+    $Result = Get-EPFirmware
+    
+    It 'calls Invoke-SSHCommandStream' {
 
-    $result = Get-EPFirmware -SSHSession 1
-
-    It "mock test" {
-      $result | Should Be 'ddd'
+      $AMCParams = @{
+        CommandName = 'Invoke-SSHCommandStream'
+        Times       = 1
+        Exactly     = $true
+      }
+      Assert-MockCalled @AMCParams
     }
   }
 }
