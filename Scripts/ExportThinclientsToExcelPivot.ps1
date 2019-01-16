eb3014b435dc2783d75b27c8a78a1e730111ea73
falkheiland <falkheiland@users.noreply.github.com>
2019-01-16 16:11:23 +0100

    Create ExportThinclientsToExcelPivot.ps1

---
+++ Scripts/ExportThinclientsToExcelPivot.ps1
@@ -0,0 +1,36 @@
+$UMSCredPath = 'C:\Credentials\UmsRmdb.cred'
+$UMSUser = 'rmdb'
+$UMSPassword = Get-Content $UMSCredPath | ConvertTo-SecureString
+$RootCredPath = 'C:\Credentials\TCRoot.cred'
+$RootCredential = (Import-Clixml -Path $RootCredPath)
+
+$PSDefaultParameterValues = @{
+  '*-UMS*:Credential'   = (New-Object System.Management.Automation.PsCredential($UMSUser, $UMSPassword))
+  '*-UMS*:Computername' = 'srvums02.bfw.local'
+}
+
+$WebSession = New-UMSAPICookie
+
+$PSDefaultParameterValues += @{
+  '*-UMS*:WebSession' = $WebSession
+}
+
+$TCColl = Get-UMSThinclient -Details full
+
+$ExportExcelParams = @{
+  Path              = 'C:\Temp\demo.xlsx'
+  KillExcel         = $true
+  WorksheetName     = 'Endpoints'
+  ClearSheet        = $true
+  IncludePivotTable = $true
+  PivotRows         = 'productId'
+  PivotData         = @{
+    "productId" = "Count"
+  }
+  IncludePivotChart = $true
+  ChartType         = 'Pie'
+  ShowPercent       = $false
+  Show              = $true
+}
+$TCColl |
+  Export-Excel @ExportExcelParams
