73c0a3d1d772249cbd6212e2a226f528af0938ae
falkheiland <falkheiland@users.noreply.github.com>
2019-02-23 17:16:54 +0100

    Update Get-UMSThinclient.Tests.ps1

--- Tests/Get-UMSThinclient.Tests.ps1
+++ Tests/Get-UMSThinclient.Tests.ps1
@@ -6,78 +6,162 @@ $Script:FunctionName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Import-Module ( '{0}/{1}.psm1' -f $Script:ModuleRoot, $Script:ModuleName)
 
 Describe "$Script:FunctionName Unit Tests" -Tag 'UnitTests' {
+
+  BeforeAll {
+    if ($null -ne $Result)
+    {
+      Clear-Variable -Name $Result
+    }
+  }
+
+  Context "Basics" {
+
+    It "Is valid Powershell (Has no script errors)" {
+      $Content = Get-Content -Path ( '{0}\Public\{1}.ps1' -f $Script:ModuleRoot, $Script:FunctionName) -ErrorAction Stop
+      $ErrorColl = $Null
+      $Null = [System.Management.Automation.PSParser]::Tokenize($Content, [ref]$ErrorColl)
+      $ErrorColl | Should -HaveCount 0
+    }
+
+    [object[]]$params = (Get-ChildItem function:\$Script:FunctionName).Parameters.Keys
+    $KnownParameters = 'Computername', 'TCPPort', 'ApiVersion', 'WebSession', 'Facets', 'TCID'
+
+    It "Should contain our specific parameters" {
+      (@(Compare-Object -ReferenceObject $KnownParameters -DifferenceObject $params -IncludeEqual |
+            Where-Object SideIndicator -eq "==").Count) | Should Be $KnownParameters.Count
+    }
+  }
+
   InModuleScope $Script:ModuleName {
-    BeforeAll {
-      $Global:ConfirmPreference = 'None'
-      #. ( '{0}\Public\{1}.ps1' -f $Script:ModuleRoot, $Script:FunctionName)
-      $PSDefaultParameterValues = @{
-        '*:WebSession' = New-MockObject -Type 'System.Management.Automation.PSCustomObject'
-      }
+
+    $PSDefaultParameterValues = @{
+      '*:WebSession'   = New-MockObject -Type 'System.Management.Automation.PSCustomObject'
+      '*:Computername' = 'dummyserver'
     }
 
-    Context "Parameter Validation" {
+    Context "General Execution" {
 
-      [object[]]$params = (Get-ChildItem function:\$Script:FunctionName).Parameters.Keys
-      $KnownParameters = 'Computername', 'TCPPort', 'ApiVersion', 'WebSession', 'Details', 'TCID'
-
-      It "Should contain our specific parameters" {
-        (@(Compare-Object -ReferenceObject $KnownParameters -DifferenceObject $params -IncludeEqual |
-              Where-Object SideIndicator -eq "==").Count) | Should Be $KnownParameters.Count
-      }
-    }
-
-    Context "Code Execution" {
-      Mock 'New-UMSAPICookie' {}
       Mock 'Invoke-UMSRestMethodWebSession' {}
-      Mock 'Write-Output' {}
 
-      $Result = Get-UMSThinclient -Computername 'Computername' -WebSession $WebSession
+      #$Result = Get-UMSThinclient
 
-      It 'Assert New-UMSAPICookie is called exactly 1 time' {
+      It 'Get-UMSThinclient Should not throw' {
+        { Get-UMSThinclient } | Should -Not -Throw
+      }
+
+      It 'Get-UMSThinclient -Details Error -ErrorAction Stop Should throw' {
+        { Get-UMSThinclient -Details Error -ErrorAction Stop } | Should -Throw
+      }
+
+    }
+
+    Context "ParameterSetName All" {
+
+      Mock 'Invoke-UMSRestMethodWebSession' {
+        [pscustomobject]@{
+          SyncRoot = @{
+            mac        = '0123456789AB'
+            firmwareID = '1'
+            lastIP     = '192.168.0.1'
+            id         = '2'
+            name       = 'endpoint01'
+            parentID   = '3'
+            movedToBin = 'False'
+            objectType = 'tc'
+            links      = '{}'
+          }
+        }
+      }
+      Mock 'Get-UMSFunctionString' {}
+
+      $Result = Get-UMSThinclient
+
+      It 'Assert Get-UMSFunctionString is called exactly 1 time' {
         $AMCParams = @{
-          CommandName = 'New-UMSAPICookie'
+          CommandName = 'Get-UMSFunctionString'
           Times       = 1
           Exactly     = $true
         }
         Assert-MockCalled @AMCParams
       }
 
-      It 'Result should be without white spaces' {
-        $Result | Should BeExactly '1Aa.:!'
-      }
-
-      It 'Result should have type string' {
-        $Result | Should -HaveType ([string])
-      }
-
-      It 'Result Count should be 1' {
-        @($Result).Count | Should Be 1
-      }
-
-      It 'Assert Write-Output is called exactly 0 times' {
+      It 'Assert Invoke-UMSRestMethodWebSession is called exactly 1 time' {
         $AMCParams = @{
-          CommandName = 'Write-Output'
-          Times       = 0
+          CommandName = 'Invoke-UMSRestMethodWebSession'
+          Times       = 1
           Exactly     = $true
         }
         Assert-MockCalled @AMCParams
+      }
+
+      It 'Result should have type pscustomobject' {
+        $Result | Should -HaveType ([pscustomobject])
+      }
+
+      It 'Result should have 1 element' {
+        @($Result).Count | Should BeExactly 1
+      }
+
+      It 'Result.id should be exactly 2' {
+        $Result.id | Should BeExactly 2
+      }
+    }
+
+    Context "ParameterSetName ID" {
+
+      Mock 'Invoke-UMSRestMethodWebSession' {
+        [pscustomobject]@{
+          mac        = '0123456789AB'
+          firmwareID = '1'
+          lastIP     = '192.168.0.1'
+          id         = '2'
+          name       = 'endpoint01'
+          parentID   = '3'
+          movedToBin = 'False'
+          objectType = 'tc'
+          links      = '{}'
+        }
+      }
+      Mock 'Get-UMSFunctionString' {}
+
+      $Result = Get-UMSThinclient -TCID 2
+
+      It 'Assert Get-UMSFunctionString is called exactly 1 time' {
+        $AMCParams = @{
+          CommandName = 'Get-UMSFunctionString'
+          Times       = 1
+          Exactly     = $true
+        }
+        Assert-MockCalled @AMCParams
+      }
+
+      It 'Assert Invoke-UMSRestMethodWebSession is called exactly 1 time' {
+        $AMCParams = @{
+          CommandName = 'Invoke-UMSRestMethodWebSession'
+          Times       = 1
+          Exactly     = $true
+        }
+        Assert-MockCalled @AMCParams
+      }
+
+      It 'Result should have type pscustomobject' {
+        $Result | Should -HaveType ([pscustomobject])
+      }
+
+      It 'Result should have 1 element' {
+        @($Result).Count | Should BeExactly 1
+      }
+
+      It 'Result.id should be exactly 2' {
+        $Result.id | Should BeExactly 2
       }
     }
 
     Context "Error Handling" {
-      Mock 'New-UMSAPICookie' {throw 'Error'}
       Mock 'Invoke-UMSRestMethodWebSession' {throw 'Error'}
-      Mock 'Write-Output' {}
 
-      $Result = Get-UMSThinclient
-
-      It 'Assert Write-Output is called exactly 1 time' {
-        $AMCParams = @{
-          CommandName = 'Write-Output'
-          Times       = 1
-          Exactly     = $true
-        }
-        Assert-MockCalled @AMCParams
+      it 'should throw Error' {
+        { Get-UMSThinclient } | should throw 'Error'
       }
 
       It 'Result should be null or empty' {
@@ -88,16 +172,43 @@ Describe "$Script:FunctionName Unit Tests" -Tag 'UnitTests' {
 }
 
 Describe "$Script:FunctionName Integration Tests" -Tags "IntegrationTests" {
-  <#
   BeforeAll {
-    $Global:ConfirmPreference = 'None'
-    . ( '{0}\Public\{1}.ps1' -f $Script:ModuleRoot, $Script:FunctionName)
-    $PSDefaultParameterValues = @{
-      #'*:SSHSession' = New-MockObject -Type 'SSH.SshSession'
+    if ($null -ne $Result)
+    {
+      Clear-Variable -Name $Result
     }
   }
-  It "doesn't throw" {
-    { Get-UMSThinclient }  | Should Not Throw
+
+  $User = 'igelums'
+  $CredPath = 'C:\Credentials\UMSRmdb.cred'
+  $Password = Get-Content $CredPath | ConvertTo-SecureString
+  $Credential = New-Object System.Management.Automation.PSCredential($User, $Password)
+
+  $PSDefaultParameterValues = @{
+    '*-UMS*:Credential'       = $Credential
+    '*-UMS*:Computername'     = 'localhost'
+    '*-UMS*:SecurityProtocol' = 'Tls12'
   }
-  #>
+
+  $WebSession = New-UMSAPICookie -Credential $Credential
+  $PSDefaultParameterValues += @{
+    '*-UMS*:WebSession' = $WebSession
+  }
+
+  Context "ParameterSetName All" {
+
+    It "doesn't throw" {
+      { Get-UMSThinclient }  | Should Not Throw
+    }
+
+    $Result = Get-UMSThinclient
+
+    It 'Result shoul not be null or empty' {
+      $Result | Should not BeNullOrEmpty
+    }
+
+    It 'Result.id should be have type [int]' {
+      $Result[0].id | Should -HaveType [int]
+    }
+  }
 }
