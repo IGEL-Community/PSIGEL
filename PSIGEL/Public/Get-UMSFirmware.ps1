6095526f9f44688b36b9697b86b0c49263031b51
falkheiland <falkheiland@users.noreply.github.com>
2019-02-25 14:17:54 +0100

    Update Get-UMSFirmware.ps1

--- PSIGEL/Public/Get-UMSFirmware.ps1
+++ PSIGEL/Public/Get-UMSFirmware.ps1
@@ -3,10 +3,11 @@
   [CmdletBinding(DefaultParameterSetName = 'All')]
   param
   (
+    [Parameter(Mandatory)]
     [String]
     $Computername,
 
-    [ValidateRange(0, 49151)]
+    [ValidateRange(0, 65535)]
     [Int]
     $TCPPort = 8443,
 
