255b2f28f12825a51474f1d80d997607e69b7c58
falkheiland <falkheiland@users.noreply.github.com>
2019-02-26 14:45:02 +0100

    Create Get-UMSThinclientAssignment.Tests.ps1

---
+++ Tests/Get-UMSThinclientAssignment.Tests.ps1
@@ -0,0 +1,170 @@
+$Script:ProjectRoot = Resolve-Path ('{0}\..' -f $PSScriptRoot)
+$Script:ModuleRoot = Split-Path (Resolve-Path ('{0}\*\*.psm1' -f $Script:ProjectRoot))
+$Script:ModuleName = Split-Path $Script:ModuleRoot -Leaf
+$Script:ModuleManifest = Resolve-Path ('{0}/{1}.psd1' -f $Script:ModuleRoot, $Script:ModuleName)
+$Script:FunctionName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Import-Module ( '{0}/{1}.psm1' -f $Script:ModuleRoot, $Script:ModuleName)
+
+Describe "$Script:FunctionName Unit Tests" -Tag 'UnitTests' {
+
+  BeforeAll {
+    if ($null -ne $Result)
+    {
+      Clear-Variable -Name $Result
+    }
+  }
+
+  Context "Basics" {
+
+    It "Is valid Powershell (Has no script errors)" {
+      $Content = Get-Content -Path ( '{0}\Public\{1}.ps1' -f $Script:ModuleRoot, $Script:FunctionName) -ErrorAction Stop
+      $ErrorColl = $Null
+      $Null = [System.Management.Automation.PSParser]::Tokenize($Content, [ref]$ErrorColl)
+      $ErrorColl | Should -HaveCount 0
+    }
+
+    [object[]]$params = (Get-ChildItem function:\$Script:FunctionName).Parameters.Keys
+    $KnownParameters = 'Computername', 'TCPPort', 'ApiVersion', 'SecurityProtocol', 'WebSession', 'TCID'
+
+    It "Should contain our specific parameters" {
+      (@(Compare-Object -ReferenceObject $KnownParameters -DifferenceObject $params -IncludeEqual |
+            Where-Object SideIndicator -eq "==").Count) | Should Be $KnownParameters.Count
+    }
+  }
+
+  InModuleScope $Script:ModuleName {
+
+    $PSDefaultParameterValues = @{
+      '*:WebSession'                     = New-MockObject -Type 'System.Management.Automation.PSCustomObject'
+      '*:Computername'                   = 'dummyserver'
+      'Get-UMSThinclientAssignment:TCID' = 2
+    }
+
+    Context "General Execution" {
+
+      Mock 'Invoke-UMSRestMethodWebSession' {}
+
+      It 'Get-UMSThinclientAssignment Should not throw' {
+        { Get-UMSThinclientAssignment } | Should -Not -Throw
+      }
+
+      It 'Get-UMSThinclientAssignment -ApiVersion 10 Stop Should throw' {
+        { Get-UMSThinclientAssignment -ApiVersion 10 -ErrorAction Stop } | Should -Throw
+      }
+
+    }
+
+    Context "All" {
+
+      Mock 'Invoke-UMSRestMethodWebSession' {
+        (
+          [pscustomobject]@{
+            assignee           = @{
+              id   = '2'
+              type = 'profile'
+            }
+            receiver           = @{
+              id   = '2'
+              type = 'tc'
+            }
+            assignmentPosition = 0
+          }
+        )
+      }
+
+      $Result = Get-UMSThinclientAssignment -TCID 2
+
+      It 'Assert Invoke-UMSRestMethodWebSession is called exactly 1 time' {
+        $AMCParams = @{
+          CommandName = 'Invoke-UMSRestMethodWebSession'
+          Times       = 1
+          Exactly     = $true
+        }
+        Assert-MockCalled @AMCParams
+      }
+
+      It 'Result should have type pscustomobject' {
+        $Result | Should -HaveType ([pscustomobject])
+      }
+
+      It 'Result should have 1 element' {
+        @($Result).Count | Should BeExactly 1
+      }
+
+      It 'Result[0] should have type [pscustomobject]' {
+        $Result[0] | Should -HaveType [pscustomobject]
+      }
+
+      It 'Result[0].assigneeid should be exactly 2' {
+        $Result[0].assigneeid | Should BeExactly 2
+      }
+
+      It 'Result[0].assigneeid should have type [int]' {
+        $Result[0].assigneeid | Should -HaveType [int]
+      }
+
+    }
+
+    Context "Error Handling" {
+      Mock 'Invoke-UMSRestMethodWebSession' {throw 'Error'}
+
+      it 'should throw Error' {
+        { Get-UMSThinclientAssignment } | should throw 'Error'
+      }
+
+      It 'Result should be null or empty' {
+        $Result | Should BeNullOrEmpty
+      }
+    }
+  }
+}
+
+Describe "$Script:FunctionName Integration Tests" -Tags "IntegrationTests" {
+  BeforeAll {
+    if ($null -ne $Result)
+    {
+      Clear-Variable -Name $Result
+    }
+  }
+
+  $User = 'igelums'
+  $CredPath = 'C:\Credentials\UMSRmdb.cred'
+  $Password = Get-Content $CredPath | ConvertTo-SecureString
+  $Credential = New-Object System.Management.Automation.PSCredential($User, $Password)
+
+  $PSDefaultParameterValues = @{
+    '*-UMS*:Credential'       = $Credential
+    '*-UMS*:Computername'     = 'localhost'
+    '*-UMS*:SecurityProtocol' = 'Tls12'
+  }
+
+  $WebSession = New-UMSAPICookie -Credential $Credential
+  $PSDefaultParameterValues += @{
+    '*-UMS*:WebSession' = $WebSession
+  }
+
+  Context "ParameterSetName All" {
+
+    It "doesn't throw" {
+      { Get-UMSThinclientAssignment -TCID 69} | Should Not Throw
+    }
+
+    $Result = Get-UMSThinclientAssignment -TCID 69
+
+    It 'Result should not be null or empty' {
+      $Result | Should not BeNullOrEmpty
+    }
+
+    It 'Result.id should be have type [int]' {
+      $Result[0].assigneeId | Should -HaveType [int]
+    }
+
+    It 'Result.assigneeType should be have type [string]' {
+      $Result[0].assigneeType | Should -HaveType [string]
+    }
+
+    It 'Result.assigneeType should not be null or empty' {
+      $Result[0].assigneeType | Should -Not -BeNullOrEmpty
+    }
+  }
+}
