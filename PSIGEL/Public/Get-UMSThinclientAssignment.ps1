b3ee8a0452124a97b261bd7f823837e38efca939
falkheiland <falkheiland@users.noreply.github.com>
2019-02-26 14:45:09 +0100

    Update Get-UMSThinclientAssignment.ps1

--- PSIGEL/Public/Get-UMSThinclientAssignment.ps1
+++ PSIGEL/Public/Get-UMSThinclientAssignment.ps1
@@ -22,7 +22,7 @@
     [Parameter(Mandatory)]
     $WebSession,
 
-    [Parameter(Mandatory, ValueFromPipeline)]
+    [Parameter(ValueFromPipeline, Mandatory)]
     [int]
     $TCID
   )
@@ -30,19 +30,35 @@
   Begin
   {
     $UriArray = @($Computername, $TCPPort, $ApiVersion)
-    $BaseURL = ('https://{0}:{1}/umsapi/v{2}/thinclients/' -f $UriArray)
+    $BaseURL = ('https://{0}:{1}/umsapi/v{2}/thinclients' -f $UriArray)
   }
   Process
   {
     $Params = @{
       WebSession       = $WebSession
-      Uri              = '{0}{1}/assignments/profiles' -f $BaseURL, $TCID
+      Uri              = '{0}/{1}/assignments/profiles' -f $BaseURL, $TCID
       Method           = 'Get'
       ContentType      = 'application/json'
       Headers          = @{}
       SecurityProtocol = ($SecurityProtocol -join ',')
     }
-    Invoke-UMSRestMethodWebSession @Params
+    $Json = Invoke-UMSRestMethodWebSession @Params
+    $Result = foreach ($item in $Json)
+    {
+      $ProfileColl = foreach ($child in $item)
+      {
+        $ProfileProperties = [ordered]@{
+          'assigneeId'         = [int]$child.assignee.id
+          'assigneeType'       = [string]$child.assignee.type
+          'receiverId'         = [int]$child.receiver.id
+          'receiverType'       = [string]$child.receiver.type
+          'assignmentPosition' = [int]$child.assignmentPosition
+        }
+        New-Object psobject -Property $ProfileProperties
+      }
+      $ProfileColl
+    }
+    $Result
   }
   End
   {
