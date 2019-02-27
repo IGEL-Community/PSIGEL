747a81de808733973a5d5d20fab3d2eddece7570
falkheiland <falkheiland@users.noreply.github.com>
2019-02-27 15:53:57 +0100

    Update New-UMSThinclient.ps1

--- PSIGEL/Public/New-UMSThinclient.ps1
+++ PSIGEL/Public/New-UMSThinclient.ps1
@@ -15,45 +15,60 @@
     [Int]
     $ApiVersion = 3,
 
-    $WebSession,
+    [ValidateSet('Tls12', 'Tls11', 'Tls', 'Ssl3')]
+    [String[]]
+    $SecurityProtocol = 'Tls12',
 
     [Parameter(Mandatory)]
+    $WebSession,
+
+    [Parameter(Mandatory, ValueFromPipelineByPropertyName, ValueFromPipeline)]
     [ValidatePattern('^([0-9a-f]{12})$')]
     [String]
     $Mac,
 
-    [Parameter(Mandatory)]
+    [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
     [int]
-    $FirmwareID,
+    $FirmwareId,
 
+    [Parameter(ValueFromPipelineByPropertyName)]
     [String]
     $Name,
 
+    [Parameter(ValueFromPipelineByPropertyName)]
     [int]
-    $ParentID = -1,
+    $ParentId = -1,
 
+    [Parameter(ValueFromPipelineByPropertyName)]
     [String]
     $Site,
 
+    [Parameter(ValueFromPipelineByPropertyName)]
     [String]
     $Department,
 
+    [Parameter(ValueFromPipelineByPropertyName)]
     [String]
     $CostCenter,
 
+    [Parameter(ValueFromPipelineByPropertyName)]
     [ValidateScript( {$_ -match [IPAddress]$_})]
     [String]
     $LastIP,
 
+    [Parameter(ValueFromPipelineByPropertyName)]
     [String]
     $Comment,
 
+    [Parameter(ValueFromPipelineByPropertyName)]
     [String]
-    $AssetID,
+    $AssetId,
 
+    [Parameter(ValueFromPipelineByPropertyName)]
     [String]
     $InserviceDate,
 
+    [Parameter(ValueFromPipelineByPropertyName)]
     [ValidateLength(19, 19)]
     [String]
     $SerialNumber
@@ -61,44 +76,49 @@
 
   Begin
   {
+    $UriArray = @($Computername, $TCPPort, $ApiVersion)
+    $BaseURL = ('https://{0}:{1}/umsapi/v{2}/thinclients' -f $UriArray)
   }
   Process
   {
-    if ($null -eq $WebSession)
-    {
-      $WebSession = New-UMSAPICookie -Computername $Computername
-    }
-
-    $UriArray = @($Computername, $TCPPort, $ApiVersion)
-    $Uri = 'https://{0}:{1}/umsapi/v{2}/thinclients/' -f $UriArray
     $Body = ConvertTo-Json @{
       mac           = $Mac
-      firmwareID    = $FirmwareID
+      firmwareID    = $FirmwareId.ToString()
       name          = $Name
-      parentID      = $ParentID
+      parentID      = $ParentId.ToString()
       site          = $Site
       department    = $Department
       costCenter    = $CostCenter
       lastIP        = $LastIP
       comment       = $Comment
-      assetID       = $AssetID
+      assetID       = $AssetId
       inserviceDate = $InserviceDate
       serialNumber  = $SerialNumber
     }
-
     $Params = @{
-      WebSession  = $WebSession
-      Uri         = $Uri
-      Body        = $Body
-      Method      = 'Put'
-      ContentType = 'application/json'
-      Headers     = @{}
+      WebSession       = $WebSession
+      Uri              = $BaseURL
+      Body             = $Body
+      Method           = 'Put'
+      ContentType      = 'application/json'
+      Headers          = @{}
+      SecurityProtocol = ($SecurityProtocol -join ',')
     }
-
-    if ($PSCmdlet.ShouldProcess('MAC-Address: {0}' -f $Mac))
+    if ($PSCmdlet.ShouldProcess('MAC: {0}' -f $Mac))
     {
-      Invoke-UMSRestMethodWebSession @Params
+      $Json = Invoke-UMSRestMethodWebSession @Params
     }
+    $Result = foreach ($item in $Json)
+    {
+      $Properties = [ordered]@{
+        'Message'  = [string]$item.message
+        'Id'       = [int]$item.id
+        'Name'     = [string]$item.name
+        'ParentId' = [int]$item.parentID
+      }
+      New-Object psobject -Property $Properties
+    }
+    $Result
   }
   End
   {
