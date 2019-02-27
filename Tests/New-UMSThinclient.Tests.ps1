4d28ebfdc717c02f7c1c06df0ce6fa669740954e
falkheiland <falkheiland@users.noreply.github.com>
2019-02-27 15:53:52 +0100

    Create New-UMSThinclient.Tests.ps1

---
+++ Tests/New-UMSThinclient.Tests.ps1
@@ -0,0 +1,202 @@
+$Script:ProjectRoot = Resolve-Path ('{0}\..' -f $PSScriptRoot)
+$Script:ModuleRoot = Split-Path (Resolve-Path ('{0}\*\*.psm1' -f $Script:ProjectRoot))
+$Script:ModuleName = Split-Path $Script:ModuleRoot -Leaf
+$Script:ModuleManifest = Resolve-Path ('{0}/{1}.psd1' -f $Script:ModuleRoot, $Script:ModuleName)
+$Script:FunctionName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Import-Module ( '{0}/{1}.psm1' -f $Script:ModuleRoot, $Script:ModuleName)
+
+Describe "$Script:FunctionName Unit Tests" -Tag 'UnitTests' {
+
+  BeforeAll {
+    if ($null -ne $Result)
+    {
+      Clear-Variable -Name $Result
+    }
+  }
+
+  Context "Basics" {
+
+    It "Is valid Powershell (Has no script errors)" {
+      $Content = Get-Content -Path ( '{0}\Public\{1}.ps1' -f $Script:ModuleRoot, $Script:FunctionName) -ErrorAction Stop
+      $ErrorColl = $Null
+      $Null = [System.Management.Automation.PSParser]::Tokenize($Content, [ref]$ErrorColl)
+      $ErrorColl | Should -HaveCount 0
+    }
+
+    [object[]]$Params = (Get-ChildItem function:\$Script:FunctionName).Parameters.Keys
+    $KnownParameters = 'Computername', 'TCPPort', 'ApiVersion', 'SecurityProtocol', 'WebSession', 'Mac', 'FirmwareId', 'Name',
+    'ParentId', 'Site', 'Department', 'CostCenter', 'LastIP', 'Comment', 'AssetId', 'InserviceDate', 'SerialNumber'
+
+    It "Should contain our specific parameters" {
+      (@(Compare-Object -ReferenceObject $KnownParameters -DifferenceObject $Params -IncludeEqual |
+            Where-Object SideIndicator -eq "==").Count) | Should Be $KnownParameters.Count
+    }
+  }
+
+  InModuleScope $Script:ModuleName {
+
+    $PSDefaultParameterValues = @{
+      '*:WebSession'   = New-MockObject -Type 'System.Management.Automation.PSCustomObject'
+      '*:Computername' = 'dummyserver'
+      '*:Confirm'      = $false
+    }
+
+    Context "General Execution" {
+
+      Mock 'Invoke-UMSRestMethodWebSession' {}
+
+      It "New-UMSThinclient -Mac '001122334455' -FirmwareId 2 -LastIP '192.168.0.1' Should not throw" {
+        { New-UMSThinclient -Mac '001122334455' -FirmwareId 2 -LastIP '192.168.0.1' } | Should -Not -Throw
+      }
+    }
+
+    Context "All" {
+
+      Mock 'Invoke-UMSRestMethodWebSession' {
+        (
+          [pscustomobject]@{
+            message  = 'Thin client successfully inserted.'
+            id       = '2'
+            name     = 'NewName'
+            parentID = -1
+          }
+        )
+      }
+
+      $Result = New-UMSThinclient -Mac '001122334455' -FirmwareId 2 -LastIP '192.168.0.1'
+
+      It 'Assert Invoke-UMSRestMethodWebSession is called exactly 1 time' {
+        $AMCParams = @{
+          CommandName = 'Invoke-UMSRestMethodWebSession'
+          Times       = 1
+          Exactly     = $true
+        }
+        Assert-MockCalled @AMCParams
+      }
+
+      It 'Result should have type pscustomobject' {
+        $Result | Should -HaveType ([pscustomobject])
+      }
+
+      It 'Result should have 1 element' {
+        @($Result).Count | Should BeExactly 1
+      }
+
+      It 'Result.Id should be exactly 2' {
+        $Result.Id | Should Be 2
+      }
+
+      It 'Result.Id should have type [int]' {
+        $Result.Id | Should -HaveType [int]
+      }
+    }
+
+    Context "Whatif" {
+
+      Mock 'Invoke-UMSRestMethodWebSession' {}
+
+      $Result = New-UMSThinclient -Mac '001122334455' -FirmwareId 2 -LastIP '192.168.0.1' -WhatIf
+
+      It 'Assert Invoke-UMSRestMethodWebSession is called exactly 0 times' {
+        $AMCParams = @{
+          CommandName = 'Invoke-UMSRestMethodWebSession'
+          Times       = 0
+          Exactly     = $true
+        }
+        Assert-MockCalled @AMCParams
+      }
+
+      It 'Result should be null or empty' {
+        $Result | Should BeNullOrEmpty
+      }
+    }
+
+    Context "Confirm" {
+
+      Mock 'Invoke-UMSRestMethodWebSession' {
+        (
+          [pscustomobject]@{
+            message  = 'Thin client successfully inserted.'
+            id       = '2'
+            name     = 'NewName'
+            parentID = -1
+          }
+        )
+      }
+
+      $Result = New-UMSThinclient -Mac '001122334455' -FirmwareId 2 -LastIP '192.168.0.1' -Confirm:$true
+
+      It 'Assert Invoke-UMSRestMethodWebSession is called exactly 0 times' {
+        $AMCParams = @{
+          CommandName = 'Invoke-UMSRestMethodWebSession'
+          Times       = 0
+          Exactly     = $true
+        }
+        Assert-MockCalled @AMCParams
+      }
+
+      It 'Result should be null or empty' {
+        $Result | Should BeNullOrEmpty
+      }
+    }
+
+    Context "Error Handling" {
+      Mock 'Invoke-UMSRestMethodWebSession' {throw 'Error'}
+
+      It "New-UMSThinclient -Mac '001122334455' -FirmwareId 2 -LastIP '192.168.0.1' -ApiVersion 10 -ErrorAction Stop Should throw" {
+        { New-UMSThinclient -Mac '001122334455' -FirmwareId 2 -LastIP '192.168.0.1' -ApiVersion 10 -ErrorAction Stop } | Should -Throw
+      }
+
+      It 'Result should be null or empty' {
+        $Result | Should BeNullOrEmpty
+      }
+    }
+  }
+}
+
+Describe "$Script:FunctionName Integration Tests" -Tags "IntegrationTests" {
+  BeforeAll {
+    if ($null -ne $Result)
+    {
+      Clear-Variable -Name $Result
+    }
+  }
+
+  $UMS = Get-Content -Raw -Path ('{0}\Tests\UMS.json' -f $Script:ProjectRoot) |
+    ConvertFrom-Json
+  $CredPath = $UMS.CredPath
+  $Password = Get-Content $CredPath | ConvertTo-SecureString
+  $Credential = New-Object System.Management.Automation.PSCredential($UMS.User, $Password)
+  $Name = 'NewProfileDirectory'
+
+  $PSDefaultParameterValues = @{
+    '*-UMS*:Credential'       = $Credential
+    '*-UMS*:Computername'     = $UMS.Computername
+    '*-UMS*:SecurityProtocol' = $UMS.SecurityProtocol
+    '*-UMS*:Name'             = $Name
+  }
+
+  $WebSession = New-UMSAPICookie -Credential $Credential
+  $PSDefaultParameterValues += @{
+    '*-UMS*:WebSession' = $WebSession
+  }
+
+  Context "ParameterSetName All" {
+
+    It "doesn't throw" {
+      { $Script:Result = New-UMSThinclient -Mac '001122334455' -FirmwareId 2 -LastIP '192.168.0.1' } | Should Not Throw
+    }
+
+    It 'Result should not be null or empty' {
+      $Result | Should not BeNullOrEmpty
+    }
+
+    It 'Result.Id should be have type [int]' {
+      $Result[0].Id | Should -HaveType [int]
+    }
+
+    It "Result.Name should not be exactly $Name" {
+      $Result[0].Name | Should -BeExactly $Name
+    }
+  }
+}
