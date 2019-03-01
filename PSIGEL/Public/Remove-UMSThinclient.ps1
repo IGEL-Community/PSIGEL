0b1d08922db2707b03c3578b31a3e8c083851e36
falkheiland <falkheiland@users.noreply.github.com>
2019-03-01 11:30:38 +0100

    Update Remove-UMSThinclient.ps1

--- PSIGEL/Public/Remove-UMSThinclient.ps1
+++ PSIGEL/Public/Remove-UMSThinclient.ps1
@@ -1,6 +1,6 @@
 ﻿function Remove-UMSThinclient
 {
-  [cmdletbinding(SupportsShouldProcess, ConfirmImpact = 'High')]
+  [cmdletbinding(SupportsShouldProcess, ConfirmImpact = 'High', DefaultParameterSetName = 'Offline')]
   param
   (
     [Parameter(Mandatory)]
@@ -15,38 +15,77 @@
     [Int]
     $ApiVersion = 3,
 
+    [ValidateSet('Tls12', 'Tls11', 'Tls', 'Ssl3')]
+    [String[]]
+    $SecurityProtocol = 'Tls12',
+
+    [Parameter(Mandatory)]
     $WebSession,
 
-    [Parameter(Mandatory, ValueFromPipeline)]
-    [int]
-    $TCID
+    [Parameter(Mandatory, ValueFromPipelineByPropertyName, ValueFromPipeline)]
+    [Int]
+    $Id,
+
+    [Parameter(ParameterSetName = 'Online')]
+    [Switch]
+    $Online
   )
 
   Begin
   {
+    $UriArray = @($Computername, $TCPPort, $ApiVersion)
+    $BaseURL = ('https://{0}:{1}/umsapi/v{2}/thinclients' -f $UriArray)
   }
   Process
   {
-    if ($null -eq $WebSession)
-    {
-      $WebSession = New-UMSAPICookie -Computername $Computername
-    }
-
-    $UriArray = @($Computername, $TCPPort, $ApiVersion, $TCID)
-    $Uri = 'https://{0}:{1}/umsapi/v{2}/thinclients/{3}/deletetcoffline' -f $UriArray
-
     $Params = @{
-      WebSession  = $WebSession
-      Uri         = $Uri
-      Method      = 'Delete'
-      ContentType = 'application/json'
-      Headers     = @{}
+      WebSession       = $WebSession
+      Method           = 'Delete'
+      ContentType      = 'application/json'
+      Headers          = @{}
+      SecurityProtocol = ($SecurityProtocol -join ',')
     }
-
+    Switch ($PsCmdlet.ParameterSetName)
+    {
+      'Offline'
+      {
+        $Params.Add('Uri', ('{0}/{1}/deletetcoffline' -f $BaseURL, $Id))
+      }
+      'Online'
+      {
+        $Params.Add('Uri', ('{0}/{1}' -f $BaseURL, $Id))
+      }
+    }
     if ($PSCmdlet.ShouldProcess('TCID: {0}' -f $TCID))
     {
-      Invoke-UMSRestMethodWebSession @Params
+      $APIObjectColl = Invoke-UMSRestMethodWebSession @Params
     }
+    $Result = foreach ($APIObject in $APIObjectColl)
+    {
+      Switch ($PsCmdlet.ParameterSetName)
+      {
+        'Offline'
+        {
+          $Properties = [ordered]@{
+            'Message' = [string]('{0}.' -f $APIObject.Message)
+            'Id'      = [int]$Id
+          }
+        }
+        'Online'
+        {
+          $Properties = [ordered]@{
+            'Message'  = [string]('{0}.' -f $APIObject.CommandExecList.message)
+            'Id'       = [int]$Id
+            'ExecId'   = [int]$APIObject.CommandExecList.execID
+            'Mac'      = [string]$APIObject.CommandExecList.mac
+            'ExecTime' = [int]$APIObject.CommandExecList.exectime
+            'State'    = [string]$APIObject.CommandExecList.state
+          }
+        }
+      }
+      New-Object psobject -Property $Properties
+    }
+    $Result
   }
   End
   {
