ed81b5ddd52a861a1884444bca9f28d7bde25134
falkheiland <falkheiland@users.noreply.github.com>
2019-03-02 15:34:10 +0100

    Create Update-UMSProfile.ps1

---
+++ PSIGEL/Public/Update-UMSProfile.ps1
@@ -0,0 +1,70 @@
+﻿function Update-UMSProfile
+{
+  [cmdletbinding(SupportsShouldProcess, ConfirmImpact = 'Low')]
+  param
+  (
+    [Parameter(Mandatory)]
+    [String]
+    $Computername,
+
+    [ValidateRange(0, 65535)]
+    [Int]
+    $TCPPort = 8443,
+
+    [ValidateSet(3)]
+    [Int]
+    $ApiVersion = 3,
+
+    [ValidateSet('Tls12', 'Tls11', 'Tls', 'Ssl3')]
+    [String[]]
+    $SecurityProtocol = 'Tls12',
+
+    [Parameter(Mandatory)]
+    $WebSession,
+
+    [Parameter(Mandatory, ValueFromPipelineByPropertyName, ValueFromPipeline)]
+    [int]
+    $Id,
+
+    [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+    [String]
+    $Name
+  )
+
+  Begin
+  {
+    $UriArray = @($Computername, $TCPPort, $ApiVersion, $Id)
+    $BaseURL = ('https://{0}:{1}/umsapi/v{2}/profiles/{3}' -f $UriArray)
+  }
+  Process
+  {
+    $Body = ConvertTo-Json @{
+      name = $Name
+    }
+    $Params = @{
+      WebSession       = $WebSession
+      Uri              = $BaseURL
+      Body             = $Body
+      Method           = 'Put'
+      ContentType      = 'application/json'
+      Headers          = @{}
+      SecurityProtocol = ($SecurityProtocol -join ',')
+    }
+    if ($PSCmdlet.ShouldProcess(('Id: {0}' -f $Id)))
+    {
+      $APIObjectColl = Invoke-UMSRestMethodWebSession @Params
+    }
+    $Result = foreach ($APIObject in $APIObjectColl)
+    {
+      $Properties = [ordered]@{
+        'Message' = [string]('{0}.' -f $APIObject.message)
+        'Id'      = [int]$Id
+      }
+      New-Object psobject -Property $Properties
+    }
+    $Result
+  }
+  End
+  {
+  }
+}
