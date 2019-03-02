3b0e8e79160a4dd79520382a2ce4088f8020b4f6
falkheiland <falkheiland@users.noreply.github.com>
2019-03-02 15:34:26 +0100

    Create Update-UMSProfileDirectory.Tests.ps1

---
+++ Tests/Update-UMSProfileDirectory.Tests.ps1
@@ -0,0 +1,210 @@
+$Script:ProjectRoot = Resolve-Path ('{0}\..' -f $PSScriptRoot)
+$Script:ModuleRoot = Split-Path (Resolve-Path ('{0}\*\*.psm1' -f $Script:ProjectRoot))
+$Script:ModuleName = Split-Path $Script:ModuleRoot -Leaf
+$Script:ModuleManifest = Resolve-Path ('{0}/{1}.psd1' -f $Script:ModuleRoot, $Script:ModuleName)
+$Script:FunctionName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Import-Module ( '{0}/{1}.psm1' -f $Script:ModuleRoot, $Script:ModuleName)
+
+Describe "$Script:FunctionName Unit Tests" -Tag 'UnitTests' {
+
+  BeforeAll {
+    if ($null -ne $Result)
+    {
+      Clear-Variable -Name $Result
+    }
+  }
+
+  Context "Basics" {
+
+    It "Is valid Powershell (Has no script errors)" {
+      $Content = Get-Content -Path ( '{0}\Public\{1}.ps1' -f $Script:ModuleRoot, $Script:FunctionName) -ErrorAction Stop
+      $ErrorColl = $Null
+      $Null = [System.Management.Automation.PSParser]::Tokenize($Content, [ref]$ErrorColl)
+      $ErrorColl | Should -HaveCount 0
+    }
+
+    [object[]]$params = (Get-ChildItem function:\$Script:FunctionName).Parameters.Keys
+    $KnownParameters = 'Computername', 'TCPPort', 'ApiVersion', 'SecurityProtocol', 'WebSession', 'Id', 'Name'
+
+    It "Should contain our specific parameters" {
+      (@(Compare-Object -ReferenceObject $KnownParameters -DifferenceObject $params -IncludeEqual |
+            Where-Object SideIndicator -eq "==").Count) | Should Be $KnownParameters.Count
+    }
+  }
+
+  InModuleScope $Script:ModuleName {
+
+    $PSDefaultParameterValues = @{
+      '*:WebSession'   = New-MockObject -Type 'System.Management.Automation.PSCustomObject'
+      '*:Computername' = 'dummyserver'
+    }
+
+    Context "General Execution" {
+
+      Mock 'Invoke-UMSRestMethodWebSession' {}
+
+      It "Update-UMSProfileDirectory -Id 2 -Name 'NewName' Should not throw" {
+        { Update-UMSProfileDirectory -Id 2 -Name 'NewName' } | Should -Not -Throw
+      }
+
+      It "Update-UMSProfileDirectory -Id 2 -Name 'NewName' -ApiVersion 10 Stop Should throw" {
+        { Update-UMSProfileDirectory -Id 2 -Name 'NewName' -ApiVersion 10 -ErrorAction Stop } | Should -Throw
+      }
+
+    }
+
+    Context "All" {
+
+      Mock 'Invoke-UMSRestMethodWebSession' {
+        (
+          [pscustomobject]@{
+            message = 'Updated directory successfully.'
+          }
+        )
+      }
+
+      $Result = Update-UMSProfileDirectory -Id 2 -Name 'NewName'
+
+      It 'Assert Invoke-UMSRestMethodWebSession is called exactly 1 time' {
+        $AMCParams = @{
+          CommandName = 'Invoke-UMSRestMethodWebSession'
+          Times       = 1
+          Exactly     = $true
+        }
+        Assert-MockCalled @AMCParams
+      }
+
+      It 'Result should have type pscustomobject' {
+        $Result | Should -HaveType ([pscustomobject])
+      }
+
+      It 'Result should have 1 element' {
+        @($Result).Count | Should BeExactly 1
+      }
+
+      It 'Result[0] should have type [pscustomobject]' {
+        $Result[0] | Should -HaveType [pscustomobject]
+      }
+
+      It 'Result[0].Id should be exactly 2' {
+        $Result[0].Id | Should BeExactly 2
+      }
+
+      It 'Result[0].Id should have type [int]' {
+        $Result[0].Id | Should -HaveType [int]
+      }
+
+      It "Result[0].Message should be exactly 'Updated directory successfully.'" {
+        $Result[0].Message | Should BeExactly 'Updated directory successfully.'
+      }
+    }
+
+    Context "Whatif" {
+
+      Mock 'Invoke-UMSRestMethodWebSession' {}
+
+      $Result = Update-UMSProfileDirectory -Id 2 -Name 'NewName' -WhatIf
+
+      It 'Assert Invoke-UMSRestMethodWebSession is called exactly 0 times' {
+        $AMCParams = @{
+          CommandName = 'Invoke-UMSRestMethodWebSession'
+          Times       = 0
+          Exactly     = $true
+        }
+        Assert-MockCalled @AMCParams
+      }
+
+      It 'Result should be null or empty' {
+        $Result | Should BeNullOrEmpty
+      }
+    }
+
+    Context "Confirm" -Tag Interactive {
+
+      Mock 'Invoke-UMSRestMethodWebSession' {
+        (
+          [pscustomobject]@{
+            message = 'Updated directory successfully.'
+          }
+        )
+      }
+
+      $Result = Update-UMSProfileDirectory -Id 2 -Name 'NewName' -Confirm:$true
+
+      It 'Assert Invoke-UMSRestMethodWebSession is called exactly 0 times' {
+        $AMCParams = @{
+          CommandName = 'Invoke-UMSRestMethodWebSession'
+          Times       = 0
+          Exactly     = $true
+        }
+        Assert-MockCalled @AMCParams
+      }
+
+      It 'Result should be null or empty' {
+        $Result | Should BeNullOrEmpty
+      }
+    }
+
+    Context "Error Handling" {
+      Mock 'Invoke-UMSRestMethodWebSession' {throw 'Error'}
+
+      it 'should throw Error' {
+        { Update-UMSProfileDirectory -Id 2 -Name 'NewName' } | should throw 'Error'
+      }
+
+      It 'Result should be null or empty' {
+        $Result | Should BeNullOrEmpty
+      }
+    }
+  }
+}
+
+Describe "$Script:FunctionName Integration Tests" -Tags "IntegrationTests" {
+  BeforeAll {
+    if ($null -ne $Result)
+    {
+      Clear-Variable -Name $Result
+    }
+  }
+
+  $User = 'igelums'
+  $CredPath = 'C:\Credentials\UMSRmdb.cred'
+  $Password = Get-Content $CredPath | ConvertTo-SecureString
+  $Credential = New-Object System.Management.Automation.PSCredential($User, $Password)
+
+  $PSDefaultParameterValues = @{
+    '*-UMS*:Credential'       = $Credential
+    '*-UMS*:Computername'     = 'localhost'
+    '*-UMS*:SecurityProtocol' = 'Tls12'
+  }
+
+  $WebSession = New-UMSAPICookie -Credential $Credential
+  $PSDefaultParameterValues += @{
+    '*-UMS*:WebSession' = $WebSession
+  }
+
+  Context "ParameterSetName All" {
+
+    It "doesn't throw" {
+      { Update-UMSProfileDirectory -Id 2 -Name 'NewName'} | Should Not Throw
+    }
+
+    $Result = Update-UMSProfileDirectory -Id 2 -Name 'NewName'
+
+    It 'Result should not be null or empty' {
+      $Result | Should not BeNullOrEmpty
+    }
+
+    It 'Result[0].Id should be have type [int]' {
+      $Result[0].Id | Should -HaveType [int]
+    }
+
+    It 'Result[0].Type should be have type [string]' {
+      $Result[0].Type | Should -HaveType [string]
+    }
+
+    It 'Result[0].Type should not be null or empty' {
+      $Result[0].Type | Should -Not -BeNullOrEmpty
+    }
+  }
+}
