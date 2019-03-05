aac9d1fbf177c8e84c9e0b729c27ea26fa1332fb
falkheiland <falkheiland@users.noreply.github.com>
2019-03-05 09:47:50 +0100

    Update Get-EPUpdateConfiguration.ps1

--- PSIGEL/Public/Get-EPUpdateConfiguration.ps1
+++ PSIGEL/Public/Get-EPUpdateConfiguration.ps1
@@ -10,40 +10,55 @@ function Get-EPUpdateConfiguration
   begin
   {
     $Command = 'cat /wfs/updateconf.ini'
-    $PatternColl = [ordered]@{
-      Protocol = 'protocol=\"(?<Protocol>.*)\"'
-      Hostname = 'hostname=\"(?<Hostname>.*)\"'
-      Port     = 'port=\"(?<Port>.*)\"'
-      Username = 'username=\"(?<Username>.*)\"'
-      Password = 'password=\"(?<Password>.*)\"'
-      Path     = 'path=\"(?<Path>.*)\"'
-    }
+    $PatternProtocol = 'protocol=\"(?<Protocol>.*)\"'
+    $PatternHostname = 'hostname=\"(?<Hostname>.*)\"'
+    $PatternPort = 'port=\"(?<Port>.*)\"'
+    $PatternUsername = 'username=\"(?<Username>.*)\"'
+    $PatternPassword = 'password=\"(?<Password>.*)\"'
+    $PatternPath = 'path=\"(?<Path>.*)\"'
   }
   process
   {
     try
     {
       $CommandResultColl = Invoke-SSHCommandStream -SSHSession $SSHSession -Command $Command
-      $Properties = [ordered]@{
-        Host = $SSHSession.Host
-      }
-      foreach ($Pattern in $PatternColl.GetEnumerator())
-      {
-        foreach ($CommandResult in $CommandResultColl)
-        {
-          if ($CommandResult -match $Pattern.Value)
-          {
-            $Properties.($Pattern.Key) = $matches.($Pattern.Key)
-          }
-        }
-      }
-      $Result = New-Object psobject -Property $Properties
-      $Result
     }
     catch
     {
       Write-Output -InputObject $PSItem.Exception.Message
     }
+    $Properties = [ordered]@{
+      Host = [String]$SSHSession.Host
+    }
+    foreach ($CommandResult in $CommandResultColl)
+    {
+      if ($CommandResult -match $PatternProtocol)
+      {
+        $Properties.Protocol = [String]$matches.Protocol
+      }
+      if ($CommandResult -match $PatternHostname)
+      {
+        $Properties.Hostname = [String]$matches.Hostname
+      }
+      if ($CommandResult -match $PatternPort)
+      {
+        $Properties.Port = [Int]$matches.Port
+      }
+      if ($CommandResult -match $PatternUsername)
+      {
+        $Properties.Username = [String]$matches.Username
+      }
+      if ($CommandResult -match $PatternPassword)
+      {
+        $Properties.Password = [String]$matches.Password
+      }
+      if ($CommandResult -match $PatternPath)
+      {
+        $Properties.Path = [String]$matches.Path
+      }
+    }
+    $Result = New-Object psobject -Property $Properties
+    $Result
   }
   end
   {
