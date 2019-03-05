e69961665584530a2546dda7592653d11e90e7ab
falkheiland <falkheiland@users.noreply.github.com>
2019-03-05 15:10:09 +0100

    Update Update-UMSThinclient.Tests.ps1

--- Tests/Update-UMSThinclient.Tests.ps1
+++ Tests/Update-UMSThinclient.Tests.ps1
@@ -159,18 +159,23 @@ Describe "$Script:FunctionName Integration Tests" -Tag "IntegrationTests" {
     }
   }
 
-  $User = 'igelums'
-  $CredPath = 'C:\Credentials\UMSRmdb.cred'
+  $UMS = Get-Content -Raw -Path ('{0}\Tests\UMS.json' -f $Script:ProjectRoot) |
+    ConvertFrom-Json
+  $CredPath = $UMS.CredPath
   $Password = Get-Content $CredPath | ConvertTo-SecureString
-  $Credential = New-Object System.Management.Automation.PSCredential($User, $Password)
+  $Credential = New-Object System.Management.Automation.PSCredential($UMS.User, $Password)
+  $Id = $UMS.UMSThinclient[1].Id
+  $CommentUpdate = $UMS.UMSThinclient[1].CommentUpdate
 
   $PSDefaultParameterValues = @{
     '*-UMS*:Credential'       = $Credential
-    '*-UMS*:Computername'     = 'localhost'
-    '*-UMS*:SecurityProtocol' = 'Tls12'
+    '*-UMS*:Computername'     = $UMS.Computername
+    '*-UMS*:SecurityProtocol' = $UMS.SecurityProtocol
+    '*-UMS*:Id'               = $Id
+    '*-UMS*:Comment'          = $CommentUpdate
   }
 
-  $WebSession = New-UMSAPICookie -Credential $Credential
+  $WebSession = New-UMSAPICookie
   $PSDefaultParameterValues += @{
     '*-UMS*:WebSession' = $WebSession
   }
@@ -178,10 +183,8 @@ Describe "$Script:FunctionName Integration Tests" -Tag "IntegrationTests" {
   Context "ParameterSetName All" {
 
     It "doesn't throw" {
-      { Update-UMSThinclient -Id 2 -Name 'NameNew'} | Should Not Throw
+      { $Script:Result = Update-UMSThinclient } | Should Not Throw
     }
-
-    $Result = Update-UMSThinclient -Id 2 -Name 'NameNew'
 
     It 'Result should not be null or empty' {
       $Result | Should not BeNullOrEmpty
@@ -191,12 +194,8 @@ Describe "$Script:FunctionName Integration Tests" -Tag "IntegrationTests" {
       $Result[0].Id | Should -HaveType [Int]
     }
 
-    It 'Result[0].Type should have type [String]' {
-      $Result[0].Type | Should -HaveType [String]
-    }
-
-    It 'Result[0].Type should not be null or empty' {
-      $Result[0].Type | Should -Not -BeNullOrEmpty
+    It "Result[0].Message should be exactly 'Update successful.'" {
+      $Result[0].Message | Should -BeExactly 'Update successful.'
     }
   }
 }
