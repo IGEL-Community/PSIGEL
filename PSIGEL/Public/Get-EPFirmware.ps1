c585a522aed44566db6761b87b1f2e35d0557791
falkheiland <falkheiland@users.noreply.github.com>
2019-03-05 10:41:11 +0100

    Update Get-EPFirmware.ps1

--- PSIGEL/Public/Get-EPFirmware.ps1
+++ PSIGEL/Public/Get-EPFirmware.ps1
@@ -16,16 +16,16 @@ function Get-EPFirmware
     try
     {
       $CommandResultColl = (Invoke-SSHCommandStream -SSHSession $SSHSession -Command $Command) -replace ('\s', '')
+      $Properties = [ordered]@{
+        'Version' = [string]$CommandResultColl
+      }
+      $Result = New-Object psobject -Property $Properties
+      $Result
     }
     catch
     {
       Write-Output -InputObject $PSItem.Exception.Message
     }
-    $Properties = [ordered]@{
-      'Version' = [string]$CommandResultColl
-    }
-    $Result = New-Object psobject -Property $Properties
-    $Result
   }
   end
   {
