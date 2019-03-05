c8575e3633e87dc1d8621d5840747d67aaf04eff
falkheiland <falkheiland@users.noreply.github.com>
2019-03-05 14:07:35 +0100

    Update Remove-UMSThinclientDirectory.Tests.ps1

--- Tests/Remove-UMSThinclientDirectory.Tests.ps1
+++ Tests/Remove-UMSThinclientDirectory.Tests.ps1
@@ -138,25 +138,26 @@ Describe "$Script:FunctionName Integration Tests" -Tag "IntegrationTests" {
   $CredPath = $UMS.CredPath
   $Password = Get-Content $CredPath | ConvertTo-SecureString
   $Credential = New-Object System.Management.Automation.PSCredential($UMS.User, $Password)
-  $ID = 552
+  $Id = $UMS.UMSThinclientDirectory[0].Id
+  $Mac = $UMS.UMSThinclientDirectory[0].Mac
 
   $PSDefaultParameterValues = @{
     '*-UMS*:Credential'       = $Credential
     '*-UMS*:Computername'     = $UMS.Computername
     '*-UMS*:SecurityProtocol' = $UMS.SecurityProtocol
-    '*-UMS*:Confirm'          = $false
     '*-UMS*:Id'               = $Id
+    '*-UMS*:Confirm'          = $false
   }
 
-  $WebSession = New-UMSAPICookie -Credential $Credential
+  $WebSession = New-UMSAPICookie
   $PSDefaultParameterValues += @{
     '*-UMS*:WebSession' = $WebSession
   }
 
-  Context "All" {
+  Context "ParameterSetName All" {
 
     It "doesn't throw" {
-      { $Script:Result = Remove-UMSProfile } | Should Not Throw
+      { $Script:Result = Remove-UMSThinclientDirectory } | Should Not Throw
     }
 
     It 'Result should not be null or empty' {
@@ -169,6 +170,10 @@ Describe "$Script:FunctionName Integration Tests" -Tag "IntegrationTests" {
 
     It "Result[0].Id should be exactly $Id" {
       $Result[0].Id | Should -BeExactly $Id
+    }
+
+    It "Result[0].Message should be exactly 'Deletion successful.'" {
+      $Result[0].Message | Should -BeExactly 'Deletion successful.'
     }
   }
 }
