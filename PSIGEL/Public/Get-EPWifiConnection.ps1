5c904d591c819fce2357e38e3570fd2c40261832
falkheiland <falkheiland@users.noreply.github.com>
2019-03-05 09:47:53 +0100

    Update Get-EPWifiConnection.ps1

--- PSIGEL/Public/Get-EPWifiConnection.ps1
+++ PSIGEL/Public/Get-EPWifiConnection.ps1
@@ -14,16 +14,16 @@ function Get-EPWifiConnection
   begin
   {
     $Command = "iwconfig $Interface"
-    $PatternColl = [ordered]@{
-      ESSID       = 'ESSID(:|=)\"(?<ESSID>.*)\"'
-      Mode        = 'Mode(:|=)(?<Mode>[^\s]*)'
-      Frequency   = 'Frequency(:|=)(?<Frequency>[^\s]*)\s(?<Unit>[^\s]*)'
-      AccessPoint = 'Access Point:\s(?<AccessPoint>[^\s]*)'
-      BitRate     = 'Bit Rate(:|=)(?<BitRate>[^\s]*)\s(?<Unit>[^\s]*)'
-      TxPower     = 'Tx-Power(:|=)(?<TxPower>[^\s]*)\s(?<Unit>[^\s]*)'
-      LinkQuality = 'Link Quality(:|=)(?<LinkQuality>[^\s]*)'
-      SignalLevel = 'Signal level(:|=)(?<SignalLevel>[^\s]*)\s(?<Unit>[^\s]*)'
-    }
+    $PatternESSID = 'ESSID(:|=)\"(?<ESSID>.*)\"'
+    $PatternMode = 'Mode(:|=)(?<Mode>[^\s]*)'
+    $PatternFrequency = 'Frequency(:|=)(?<Frequency>[^\s]*)\s(?<Unit>[^\s]*)'
+    $PatternAccessPoint = 'Access Point:\s(?<AccessPoint>[^\s]*)'
+    $PatternBitRate = 'Bit Rate(:|=)(?<BitRate>[^\s]*)\s(?<Unit>[^\s]*)'
+    $PatternTxPower = 'Tx-Power(:|=)(?<TxPower>[^\s]*)\s(?<Unit>[^\s]*)'
+    #$PatternLinkQuality = 'Link Quality(:|=)(?<LinkQuality>[^\s]*)'
+    $PatternLinkQualityActual = 'Link Quality(:|=)(?<LinkQualityActual>\d+)'
+    $PatternLinkQualityMax = 'Link Quality(:|=)\d*\/(?<LinkQualityMax>\d+)'
+    $PatternSignalLevel = 'Signal level(:|=)(?<SignalLevel>[^\s]*)\s(?<Unit>[^\s]*)'
   }
   process
   {
@@ -35,19 +35,47 @@ function Get-EPWifiConnection
     {
       Write-Output -InputObject $PSItem.Exception.Message
     }
-
     $Properties = [ordered]@{
-      Host      = $SSHSession.Host
-      Interface = $Interface
+      Host      = [String]$SSHSession.Host
+      Interface = [String]$Interface
     }
-    foreach ($Pattern in $PatternColl.GetEnumerator())
+    foreach ($CommandResult in $CommandResultColl)
     {
-      foreach ($CommandResult in $CommandResultColl)
+      if ($CommandResult -match $PatternESSID)
       {
-        if ($CommandResult -match $Pattern.Value)
-        {
-          $Properties.($Pattern.Key) = $matches.($Pattern.Key)
-        }
+        $Properties.ESSID = [String]$matches.ESSID
+      }
+      if ($CommandResult -match $PatternMode)
+      {
+        $Properties.Mode = [String]$matches.Mode
+      }
+      if ($CommandResult -match $PatternFrequency)
+      {
+        $Properties.Frequency = [Single]$matches.Frequency
+      }
+      if ($CommandResult -match $PatternAccessPoint)
+      {
+        $Properties.AccessPoint = [String]$matches.AccessPoint
+      }
+      if ($CommandResult -match $PatternBitRate)
+      {
+        $Properties.BitRate = [Int]$matches.BitRate
+      }
+      if ($CommandResult -match $PatternTxPower)
+      {
+        $Properties.TxPower = [Int]$matches.TxPower
+      }
+      if ($CommandResult -match $PatternLinkQualityActual)
+      {
+        $Properties.LinkQualityActual = [Int]$matches.LinkQualityActual
+      }
+      if ($CommandResult -match $PatternLinkQualityMax)
+      {
+        $Properties.LinkQualityMax = [Int]$matches.LinkQualityMax
+      }
+      if ($CommandResult -match $PatternSignalLevel)
+      {
+        $Properties.SignalLevel = [Int]$matches.SignalLevel
       }
     }
     $Result = New-Object psobject -Property $Properties
