e811b34f44dde41e9819c9203f7a7e5a92110e40
falkheiland <falkheiland@users.noreply.github.com>
2019-03-06 10:56:52 +0100

    Update Remove-UMSThinclientDirectory.ps1

--- PSIGEL/Public/Remove-UMSThinclientDirectory.ps1
+++ PSIGEL/Public/Remove-UMSThinclientDirectory.ps1
@@ -29,14 +29,14 @@ function Remove-UMSThinclientDirectory
 
   Begin
   {
-    $UriArray = @($Computername, $TCPPort, $ApiVersion, $Id)
-    $BaseURL = ('https://{0}:{1}/umsapi/v{2}/directories/tcdirectories/{3}' -f $UriArray)
+    $UriArray = @($Computername, $TCPPort, $ApiVersion)
+    $BaseURL = ('https://{0}:{1}/umsapi/v{2}/directories/tcdirectories' -f $UriArray)
   }
   Process
   {
     $Params = @{
       WebSession       = $WebSession
-      Uri              = $BaseURL
+      Uri              = ('{0}/{1}' -f $BaseURL, $Id)
       Method           = 'Delete'
       ContentType      = 'application/json'
       Headers          = @{}
