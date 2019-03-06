dc97f19843b238d644a2d18819836c9b4e210cb5
falkheiland <falkheiland@users.noreply.github.com>
2019-03-06 10:57:01 +0100

    Update Update-UMSThinclientDirectory.ps1

--- PSIGEL/Public/Update-UMSThinclientDirectory.ps1
+++ PSIGEL/Public/Update-UMSThinclientDirectory.ps1
@@ -33,8 +33,8 @@ function Update-UMSThinclientDirectory
 
   Begin
   {
-    $UriArray = @($Computername, $TCPPort, $ApiVersion, $Id)
-    $BaseURL = ('https://{0}:{1}/umsapi/v{2}/directories/tcdirectories/{3}' -f $UriArray)
+    $UriArray = @($Computername, $TCPPort, $ApiVersion)
+    $BaseURL = ('https://{0}:{1}/umsapi/v{2}/directories/tcdirectories' -f $UriArray)
   }
   Process
   {
@@ -43,7 +43,7 @@ function Update-UMSThinclientDirectory
     }
     $Params = @{
       WebSession       = $WebSession
-      Uri              = $BaseURL
+      Uri              = ('{0}/{1}' -f $BaseURL, $Id)
       Body             = $Body
       Method           = 'Put'
       ContentType      = 'application/json'
