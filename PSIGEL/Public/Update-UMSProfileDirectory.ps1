b7101bbc4938b3010942ab4457f45fc1284946c7
falkheiland <falkheiland@users.noreply.github.com>
2019-03-06 10:56:56 +0100

    Update Update-UMSProfileDirectory.ps1

--- PSIGEL/Public/Update-UMSProfileDirectory.ps1
+++ PSIGEL/Public/Update-UMSProfileDirectory.ps1
@@ -33,8 +33,8 @@ function Update-UMSProfileDirectory
 
   Begin
   {
-    $UriArray = @($Computername, $TCPPort, $ApiVersion, $Id)
-    $BaseURL = ('https://{0}:{1}/umsapi/v{2}/directories/profiledirectories/{3}' -f $UriArray)
+    $UriArray = @($Computername, $TCPPort, $ApiVersion)
+    $BaseURL = ('https://{0}:{1}/umsapi/v{2}/directories/profiledirectories' -f $UriArray)
   }
   Process
   {
@@ -43,7 +43,7 @@ function Update-UMSProfileDirectory
     }
     $Params = @{
       WebSession       = $WebSession
-      Uri              = $BaseURL
+      Uri              = ('{0}/{1}' -f $BaseURL, $Id)
       Body             = $Body
       Method           = 'Put'
       ContentType      = 'application/json'
