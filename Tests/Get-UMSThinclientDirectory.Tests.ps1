563d24a1aed7b0c29a715156b746db11b9db5921
falkheiland <falkheiland@users.noreply.github.com>
2019-03-06 14:47:04 +0100

    Update Get-UMSThinclientDirectory.Tests.ps1

--- Tests/Get-UMSThinclientDirectory.Tests.ps1
+++ Tests/Get-UMSThinclientDirectory.Tests.ps1
@@ -24,7 +24,7 @@ Describe "$Script:FunctionName Unit Tests" -Tag 'UnitTests' {
     }
 
     [object[]]$params = (Get-ChildItem function:\$Script:FunctionName).Parameters.Keys
-    $KnownParameters = 'Computername', 'TCPPort', 'ApiVersion', 'SecurityProtocol', 'WebSession', 'Facet', 'Id'
+    $KnownParameters = 'Computername', 'TCPPort', 'ApiVersion', 'SecurityProtocol', 'WebSession', 'Option', 'Id'
 
     It "Should contain our specific parameters" {
       (@(Compare-Object -ReferenceObject $KnownParameters -DifferenceObject $params -IncludeEqual |
@@ -135,7 +135,7 @@ Describe "$Script:FunctionName Unit Tests" -Tag 'UnitTests' {
       }
     }
 
-    Context "Facet children" {
+    Context "Option children" {
 
       Mock 'Invoke-UMSRestMethodWebSession' {
         [pscustomobject]@{
@@ -158,7 +158,7 @@ Describe "$Script:FunctionName Unit Tests" -Tag 'UnitTests' {
       }
       Mock 'New-UMSFunctionString' {}
 
-      $Result = Get-UMSThinclientDirectory -Id 2 -Facet children
+      $Result = Get-UMSThinclientDirectory -Id 2 -Option children
 
       It 'Assert New-UMSFunctionString is called exactly 1 time' {
         $AMCParams = @{
