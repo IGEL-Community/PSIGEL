4be8b05a6581ffde7eebc26ac3ee511f04781e0e
Falk <falkheiland@googlemail.com>
2019-08-05 14:46:46 +0200

    Update ImportDevices.ps1

--- Scripts/ImportDevices.ps1
+++ Scripts/ImportDevices.ps1
@@ -9,8 +9,7 @@ $PSDefaultParameterValues += @{
   '*-UMS*:WebSession' = (New-UMSAPICookie)
 }
 
-$Path = 'E:\GitHub\PSIGEL\Drafts\'
-#$Path = 'C:\Temp\'
+$Path = 'C:\Temp\'
 
 $IGELImportFile = '{0}2260123456-000010.csv' -f $Path
 $AssetExportFile = '{0}Assets.csv' -f $Path
@@ -18,7 +17,7 @@ $AssetExportFile = '{0}Assets.csv' -f $Path
 $IGELImportColl = Import-Csv -Delimiter ';' -Path $IGELImportFile -Header '0', 'SerialNumber', 'MacAddress', '3', '4'
 $AssetExportColl = Import-Csv -Delimiter ';' -Path $AssetExportFile
 
-$DevicePrefix = 'DEV'
+$DevicePrefix = 'DEV-'
 $MajorVersion = 10
 
 $FirmwareId = (((Get-UMSFirmware).where{ $_.Version.Major -eq $MajorVersion } |
@@ -33,14 +32,14 @@ $NewDeviceCollParams = @{
   RightProperties   = 'InventoryNumber'
   Type              = 'AllInLeft'
 }
-$NewDeviceColl = Join-Object @NewDeviceCollParams | Sort-Object -Property SerialNumber
+$NewDeviceColl = Join-Object @NewDeviceCollParams
 
 foreach ($NewDevice in $NewDeviceColl)
 {
   $NewDeviceParams = @{
     Mac          = $NewDevice.MacAddress
     FirmwareId   = $FirmwareId
-    Name         = '{0}-{1}' -f $DevicePrefix, $NewDevice.InventoryNumber
+    Name         = '{0}{1}' -f $DevicePrefix, $NewDevice.InventoryNumber
     SerialNumber = $NewDevice.SerialNumber
     AssetId      = $NewDevice.InventoryNumber
   }
