fe97fbb312f260d30c69d1be00f389ed7264bbcf
Falk <falkheiland@googlemail.com>
2019-08-05 12:43:13 +0200

    Update ImportDevices.ps1

--- Scripts/ImportDevices.ps1
+++ Scripts/ImportDevices.ps1
@@ -3,42 +3,46 @@ $UMSCredPath = 'C:\Credentials\UmsRmdb.cred'
 $PSDefaultParameterValues = @{
   'New-UMSAPICookie:Credential' = Import-Clixml -Path $UMSCredPath
   '*-UMS*:Computername'         = 'igelrmserver'
-  '*-UMS*:SecurityProtocol'     = 'Tls'
+  #'*-UMS*:SecurityProtocol'     = 'Tls'
 }
 $PSDefaultParameterValues += @{
   '*-UMS*:WebSession' = (New-UMSAPICookie)
 }
 
-$IGELImportFile = 'C:\Temp\2262332160-000010.csv'
-$AssetExportFile = 'C:\Temp\Geraete.csv'
+$Path = 'E:\GitHub\PSIGEL\Drafts\'
+#$Path = 'C:\Temp\'
 
-$IGELImportColl = Import-Csv -Delimiter ';' -Path $IGELImportFile
+$IGELImportFile = '{0}2260123456-000010.csv' -f $Path
+$AssetExportFile = '{0}Assets.csv' -f $Path
+
+$IGELImportColl = Import-Csv -Delimiter ';' -Path $IGELImportFile -Header '0', 'SerialNumber', 'MacAddress', '3', '4'
 $AssetExportColl = Import-Csv -Delimiter ';' -Path $AssetExportFile
 
+$DevicePrefix = 'DEV'
 $MajorVersion = 10
+
 $FirmwareId = (((Get-UMSFirmware).where{ $_.Version.Major -eq $MajorVersion } |
-    Sort-Object -Property Version -Descending)[0]).Id
+      Sort-Object -Property Version -Descending)[0]).Id
 
 $NewDeviceCollParams = @{
   Left              = $IGELImportColl
-  LeftJoinProperty  = 'Serien-Nr. '
-  LeftProperties    = 'Serien-Nr. ', 'Mac-Adressen'
+  LeftJoinProperty  = 'SerialNumber'
+  LeftProperties    = 'SerialNumber', 'MacAddress'
   Right             = $AssetExportColl
-  RightJoinProperty = 'Serien_Nr'
-  RightProperties   = 'Inventar_Nr'
+  RightJoinProperty = 'SerialNumber'
+  RightProperties   = 'InventoryNumber'
   Type              = 'AllInLeft'
 }
-$NewDeviceColl = Join-Object @NewDeviceCollParams
+$NewDeviceColl = Join-Object @NewDeviceCollParams | Sort-Object -Property SerialNumber
 
 foreach ($NewDevice in $NewDeviceColl)
 {
   $NewDeviceParams = @{
-    Mac          = $NewDevice.'Mac-Adressen'
+    Mac          = $NewDevice.MacAddress
     FirmwareId   = $FirmwareId
-    Name         = 'TC{0}' -f $NewDevice.Inventar_Nr
-    SerialNumber = $NewDevice.'Serien-Nr. '
-    ParentId     = 216020
-    AssetId      = $NewDevice.Inventar_Nr
+    Name         = '{0}-{1}' -f $DevicePrefix, $NewDevice.InventoryNumber
+    SerialNumber = $NewDevice.SerialNumber
+    AssetId      = $NewDevice.InventoryNumber
   }
   New-UMSDevice @NewDeviceParams
 }
