6ecf09da868a67407933f1daaf0c4ae7d3ac6885
falkheiland <falkheiland@users.noreply.github.com>
2020-04-17 15:00:49 +0200

    added PSCore Support

--- PSIGEL/Private/Invoke-UMSRestMethodWebSession.ps1
+++ PSIGEL/Private/Invoke-UMSRestMethodWebSession.ps1
@@ -74,33 +74,81 @@ function Invoke-UMSRestMethodWebSession
   }
   process
   {
-    try
+    switch ($PSEdition)
     {
-      Invoke-RestMethod @PSBoundParameters -ErrorAction Stop
-    }
-    catch [System.Net.WebException]
-    {
-      switch ($($PSItem.Exception.Response.StatusCode.value__))
+      'Desktop'
       {
-        400
+        try
         {
-          Write-Warning -Message ('Error executing IMI RestAPI request. Uri: {0} Method: {1}' -f $Uri, $Method)
+          Invoke-RestMethod @PSBoundParameters -ErrorAction Stop
         }
-        401
+        catch [System.Net.WebException]
         {
-          Write-Warning -Message ('Error logging in, it seems as you have entered invalid credentials. Uri: {0} Method: {1}' -f $Uri, $Method)
+          switch ($($PSItem.Exception.Response.StatusCode.value__))
+          {
+            400
+            {
+              Write-Warning -Message ('Bad Request: The request does not match the API, e.g. it uses the wrong HTTP method. Uri: {0} Method: {1}' -f $Uri, $Method)
+            }
+            401
+            {
+              Write-Warning -Message ('Unauthorized: The client has not logged in or has sent the wrong credentials. Uri: {0} Method: {1}' -f $Uri, $Method)
+            }
+            404
+            {
+              Write-Warning -Message ('Not Found: The endpoint does not exist, it may be misspelled. Uri: {0} Method: {1}' -f $Uri, $Method)
+            }
+            415
+            {
+              Write-Warning -Message ('Unsupported Media Type: The body content, e.g. JSON, does not match the Content-Type header or is not well-formed. Uri: {0} Method: {1}' -f $Uri, $Method)
+            }
+            500
+            {
+              Write-Warning -Message ('Internal Server Error: The server has encountered an error, check the server logfiles catalina.log and stderr. Uri: {0} Method: {1}' -f $Uri, $Method)
+            }
+            default
+            {
+              Write-Warning -Message ('Some error occured see HTTP status code {0} for further details. Uri: {1} Method: {2}' -f $PSItem.Exception.Response.StatusCode, $Uri, $Method)
+            }
+          }
+
         }
-        403
+      }
+      'Core'
+      {
+        try
         {
-          Write-Warning -Message ('Error logging in, it seems as you have not subscripted this version of IMI. Uri: {0} Method: {1}' -f $Uri, $Method)
+          Invoke-RestMethod @PSBoundParameters -ErrorAction Stop
         }
-        415
+        catch [Microsoft.PowerShell.Commands.HttpResponseException]
         {
-          Write-Warning -Message ('Unsupported Media Type. Uri: {0} Method: {1}' -f $Uri, $Method)
-        }
-        default
-        {
-          Write-Warning -Message ('Some error occured see HTTP status code for further details. Uri: {0} Method: {1}' -f $Uri, $Method)
+          switch ($($PSItem.Exception.Response.StatusCode.value__))
+          {
+            400
+            {
+              Write-Warning -Message ('Bad Request: The request does not match the API, e.g. it uses the wrong HTTP method. Uri: {0} Method: {1}' -f $Uri, $Method)
+            }
+            401
+            {
+              Write-Warning -Message ('Unauthorized: The client has not logged in or has sent the wrong credentials. Uri: {0} Method: {1}' -f $Uri, $Method)
+            }
+            404
+            {
+              Write-Warning -Message ('Not Found: The endpoint does not exist, it may be misspelled. Uri: {0} Method: {1}' -f $Uri, $Method)
+            }
+            415
+            {
+              Write-Warning -Message ('Unsupported Media Type: The body content, e.g. JSON, does not match the Content-Type header or is not well-formed. Uri: {0} Method: {1}' -f $Uri, $Method)
+            }
+            500
+            {
+              Write-Warning -Message ('Internal Server Error: The server has encountered an error, check the server logfiles catalina.log and stderr. Uri: {0} Method: {1}' -f $Uri, $Method)
+            }
+            default
+            {
+              Write-Warning -Message ('Some error occured see HTTP status code {0} for further details. Uri: {1} Method: {2}' -f $PSItem.Exception.Response.StatusCode, $Uri, $Method)
+            }
+          }
         }
       }
     }
