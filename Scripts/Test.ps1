a668e2fef60cab6cf41959c937b0439ac85478b1
falkheiland <falkheiland@users.noreply.github.com>
2020-04-17 15:00:52 +0200

    Update Test.ps1

--- Scripts/Test.ps1
+++ Scripts/Test.ps1
@@ -1,19 +1,35 @@
-Import-Module C:\GitHub\PSIGEL\PSIGEL\PSIGEL.psd1 -Force
-$UMSCredPath = 'C:\Credentials\UmsRmdb.cred'
+$DSC = [IO.Path]::DirectorySeparatorChar
+$PSIGELPath = 'GitHub{0}PSIGEL{0}PSIGEL{0}PSIGEL.psd1' -f $DSC
 
 $PSDefaultParameterValues = @{
-  'New-UMSAPICookie:Credential' = Import-Clixml -Path $UMSCredPath
-  '*-UMS*:Computername'         = 'igelrmserver'
-  '*-UMS*:TCPPort'              = 9443
-  '*-UMS*:Confirm'              = $False
+  '*-UMS*:Computername' = 'igelrmserver'
+  '*-UMS*:TCPPort'      = 9443
+  '*-UMS*:Confirm'      = $False
   #'*-UMS*:SecurityProtocol'     = 'Tls'
 }
+
+if (($PSEdition -eq 'Core' -and $IsWindows) -or ($PSEdition -eq 'Desktop' -and ($PSVersionTable.PSVersion.Major -eq 5 -and $PSVersionTable.PSVersion.Minor -eq 1)))
+{
+  # PS7 on Windows or Windows PowerShell 5.1
+  Import-Module -FullyQualifiedName ('C:\{0}' -f $PSIGELPath) -Force
+  $PSDefaultParameterValues.Add('New-UMSAPICookie:Credential', (Import-Clixml -Path 'C:\Credentials\UmsRmdb.cred'))
+}
+elseif ($PSEdition -eq 'core' -and (-Not $IsWindows) )
+{
+  # PS7 on Linux OR MacOS
+  Import-Module -FullyQualifiedName ('/mnt/c/{0}' -f $PSIGELPath) -Force
+  $Credential = Get-Credential
+  $PSDefaultParameterValues.Add('New-UMSAPICookie:Credential', $Credential)
+}
+
 $PSDefaultParameterValues += @{
   '*-UMS*:WebSession' = New-UMSAPICookie
 }
 
 $Result = ''
-$Result = Get-UMSDeviceAssignment -Id 505
+#$Result = Get-UMSDeviceAssignment -Id 505
+#$Result = Get-UMSDevice
+$Result = Get-UMSFirmware
 $Result
 
 
