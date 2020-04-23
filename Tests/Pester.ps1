a0bea4e24e2a6cf1b5c40a19e3f29078e5b680d6
falkheiland <falkheiland@users.noreply.github.com>
2020-04-23 12:10:28 +0200

    Update Pester.ps1

--- Tests/Pester.ps1
+++ Tests/Pester.ps1
@@ -14,17 +14,28 @@ $DSC = [IO.Path]::DirectorySeparatorChar
 $ProjectRoot = Resolve-Path ('{1}{0}..' -f $DSC, $PSScriptRoot)
 $ModuleRoot = Split-Path (Resolve-Path ('{1}{0}*{0}*.psm1' -f $DSC, $ProjectRoot))
 $ModuleName = Split-Path $ModuleRoot -Leaf
-$OutputPath = '{1}{0}Tests{0}Data' -f $DSC, $ProjectRoot
 Import-Module ( '{1}{0}{2}.psm1' -f $DSC, $Script:ModuleRoot, $Script:ModuleName) -Force
 
 $Cfg = Import-PowerShellDataFile -Path ('{1}{0}Tests{0}Config.psd1' -f $DSC, $Script:ProjectRoot)
 
-# PS7 on Windows
-if (($PSEdition -eq 'Desktop') -and ($PSVersionTable.PSVersion.Major -eq 7))
+if ($PSEdition -eq 'Desktop')
 {
-  Import-Module ('{0}\7\Modules\CimCmdlets\CimCmdlets.psd1' -f [Environment]::GetEnvironmentVariable('ProgramFiles')) -Force
+  $OutputPath = '{1}{0}Tests{0}Data{0}{2}{0}' -f $DSC, $ProjectRoot, $Cfg.OutputPath.Desktop
 }
-
+elseif ($IsWindows -and $PSEdition -eq 'Core')
+{
+  $OutputPath = '{1}{0}Tests{0}Data{0}{2}{0}' -f $DSC, $ProjectRoot, $Cfg.OutputPath.CoreW10
+  Import-Module ('{0}\PowerShell\7\Modules\CimCmdlets\CimCmdlets.psd1' -f [Environment]::GetEnvironmentVariable('ProgramFiles')) -Force
+  Import-Module -Name Pester -Force
+}
+elseif ($IsLinux)
+{
+  $OutputPath = '{1}{0}Tests{0}Data{0}{2}{0}' -f $DSC, $ProjectRoot, $Cfg.OutputPath.CoreWSL
+}
+else
+{
+  $OutputPath = '{1}{0}Tests{0}Data{0}{2}{0}' -f $DSC, $ProjectRoot
+}
 if ($Tags -contains { 'IntegrationTest ' -or 'All' })
 {
   if ($IsLinux)
@@ -56,12 +67,12 @@ foreach ($Test in $Cfg.Tests)
   $IVPParams.Add('Show', $Show)
   $IVPParams.Add('EnableExit', $EnableExit)
   $IVPParams.Add('Script', ('{1}{0}Tests{0}{2}.Tests.ps1' -f $DSC, $ProjectRoot, $Test.Name))
+  $IVPParams.Add('Outputfile', ('{1}{0}{2}.Tests.xml' -f $DSC, $OutputPath, $Test.Name))
   switch ($Test)
   {
     ( { $PSItem.CodeCoveragePath } )
     {
       $IVPParams.Add('CodeCoverage', ('{1}{0}{2}{0}{3}{0}{4}.ps1' -f $DSC, $ProjectRoot, $ModuleName, $PSItem.CodeCoveragePath, $Test.Name))
-      $IVPParams.Add('Outputfile', ('{1}{0}{2}.Tests.xml' -f $DSC, $OutputPath, $Test.Name))
     }
   }
   Invoke-Pester @IVPParams
